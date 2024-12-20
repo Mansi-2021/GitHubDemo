5d8235ee4cf763782b797017272c6a6681f63090
Mansi <bagale.manasi@gmail.com>
2024-12-20 13:27:14 +0530

    second commit

--- src/main/java/com/pages/LogiinPage.java
+++ src/main/java/com/pages/LogiinPage.java
@@ -4,7 +4,7 @@ public class LogiinPage {
 	
 	public void LogiinPage() {
 		System.out.println("THis is loginPage");
-		System.out.println("This is new comment");
+		System.out.println("This is first comment");
 	}
 
 }
